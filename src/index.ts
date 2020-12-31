ca455c71655da81c4b0c5276018888bc7197fa26
Kevin Luo <kvn.luo@gmail.com>
2020-12-30 21:42:39 -0500

    add session cookie middleware

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import { createConnection } from "typeorm"
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import express from "express";
+import connectRedis from "connect-redis";
+import Redis from "ioredis";
+import session from "express-session";
+import cors from "cors";
 
 import { League } from "./entities/League";
 import { Team } from "./entities/Team";
@@ -13,6 +17,7 @@ import { LeagueResolver } from "./resolvers/league";
 import { TeamResolver } from "./resolvers/team";
 import { PlayerResolver } from "./resolvers/player";
 import { MyContext } from "./types";
+import { COOKIE_NAME, __prod__ } from "./constants";
 
 const main = async () => {
 
@@ -26,12 +31,36 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redis = new Redis(process.env.REDIS_URL);
+
+  app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }))
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({ client: redis, disableTTL: true, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__,
+      },
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, LeagueResolver, TeamResolver, PlayerResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext =>({ req, res })
+    context: ({ req, res }): MyContext =>({ req, res, redis })
   })
 
   apolloServer.applyMiddleware({ app });
